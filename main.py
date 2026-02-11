48ffe20d3100c8533bdf85d09bfdd628263b9e2b
FabricioFernandezz <fabriciofernandezz2001@gmail.com>
2026-02-11 17:21:34 -0300

    Refactor image processing logic into a function and add command-line arguments for mode and file path

--- main.py
+++ main.py
@@ -1,29 +1,11 @@
 import cv2 as cv
 import mediapipe as mp
 import os
+import argparse
 
-img_path = ".\\resources\\images\\test_image.jpg"
-
-img = cv.imread(img_path)
-
-output_dir = "./resources/output"
-
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
-
-# cv.imshow(" test_image", img)
-
-H, W , _ = img.shape #ignoramos los canales
-
-mp_face_detection = mp.solutions.face_detection
-
-
-with mp_face_detection.FaceDetection(0,0.5) as face_detection:
+def process_img(img, face_detection):
     img_rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
     out = face_detection.process(img_rgb)
-
-    print(out.detections)
-
 
     if out.detections is not None:
     # recorremos solamente detection, ya que este tiene una variable que nos importa, esta es location_data, la cual es un diccionario con las coordenadas de la cara detectada
@@ -45,5 +27,26 @@ with mp_face_detection.FaceDetection(0,0.5) as face_detection:
     elif out.detections is None:
         print("no se detecto ninguna cara")
 
+    return img
 
-cv.imwrite(os.path.join(output_dir,"output.png"), img)
+arg = argparse.ArgumentParser() #creamos un objeto de la clase ArgumentParser, esta clase nos permite crear argumentos para nuestro programa, es decir, podemos pasarle argumentos desde la linea de comandos para que nuestro programa haga cosas diferentes dependiendo de los argumentos que le pasemos
+
+arg.add_argument("--mode", default="image") # agregamos un argumento llamado mode, este argumento nos va a permitir elegir entre procesar una imagen o un video, por defecto se va a procesar una imagen, pero si queremos procesar un video, podemos pasarle el argumento --mode video
+arg.add_argument("--filePath", default=".\\resources\\images\\test_image.jpg") # agregamos un argumento llamado filePath, este argumento nos va a permitir elegir la ruta del archivo que queremos procesar, por defecto se va a procesar una imagen que se encuentra en la carpeta resources/images/test_image.jpg, pero si queremos procesar otro archivo, podemos pasarle el argumento --filePath seguido de la ruta del archivo que queremos procesar
+
+output_dir = "./resources/output"
+
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+mp_face_detection = mp.solutions.face_detection
+
+with mp_face_detection.FaceDetection(0,0.5) as face_detection:
+    if arg.parse_args().mode == "image": # si el argumento mode es igual a image, entonces se va a procesar una imagen
+    
+        img = cv.imread(arg.parse_args().filePath)
+        H,W,_ = img.shape
+
+        img= process_img(img,face_detection)
+
+        cv.imwrite(os.path.join(output_dir,"output.png"), img)
